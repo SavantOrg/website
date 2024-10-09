aaa9ae290e908fbe22eb5c02ff0cfb21099cd43d
Ramon Perez <ramonpzg@protonmail.com>
2024-10-09 17:12:46 +1100

    changed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-    site: 'https://savant.sh',
-    base: 'website',
+    site: 'https://savantorg.github.io',
   })
